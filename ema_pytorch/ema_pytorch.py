3771a8fce853e30907e6fd60571a36a37c42a89c
Phil Wang <lucidrains@gmail.com>
2022-06-20 11:41:08 -0700

    bug

--- ema_pytorch/ema_pytorch.py
+++ ema_pytorch/ema_pytorch.py
@@ -2,6 +2,16 @@ import copy
 import torch
 from torch import nn
 
+def clamp(value, min_value = None, max_value = None):
+    assert exists(min_value) or exists(max_value)
+    if exists(min_value):
+        value = max(value, min_value)
+
+    if exists(max_value):
+        value = min(value, max_value)
+
+    return value
+
 class EMA(nn.Module):
     """
     Implements exponential moving average shadowing for your model.
