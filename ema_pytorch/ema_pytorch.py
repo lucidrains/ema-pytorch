3edece4321dc521ddf4e39932270d2def4d8a8b9
Phil Wang <lucidrains@gmail.com>
2023-11-03 08:29:33 -0700

    support keeping the EMA model on cpu, autoconverting device

--- ema_pytorch/ema_pytorch.py
+++ ema_pytorch/ema_pytorch.py
@@ -1,7 +1,8 @@
-import copy
+from copy import deepcopy
+from functools import partial
 
 import torch
-from torch import nn
+from torch import nn, Tensor
 from torch.nn import Module
 
 from beartype import beartype
@@ -9,6 +10,21 @@ from beartype.typing import Set, Optional
 
 def exists(val):
     return val is not None
+
+def get_module_device(m: Module):
+    return next(m.parameters()).device
+
+def inplace_copy(src: Tensor, tgt: Tensor, *, auto_move_device = False):
+    if auto_move_device:
+        tgt = tgt.to(src.device)
+
+    src.copy_(tgt)
+
+def inplace_lerp(src: Tensor, tgt: Tensor, weight, *, auto_move_device = False):
+    if auto_move_device:
+        tgt = tgt.to(src.device)
+
+    src.lerp_(tgt, weight)
 
 class EMA(Module):
     """
@@ -45,7 +61,8 @@ class EMA(Module):
         param_or_buffer_names_no_ema: Set[str] = set(),
         ignore_names: Set[str] = set(),
         ignore_startswith_names: Set[str] = set(),
-        include_online_model = True  # set this to False if you do not wish for the online model to be saved along with the ema model (managed externally)
+        include_online_model = True,                  # set this to False if you do not wish for the online model to be saved along with the ema model (managed externally)
+        allow_different_devices = False               # if the EMA model is on a different device (say CPU), automatically move the tensor
     ):
         super().__init__()
         self.beta = beta
@@ -65,15 +82,24 @@ class EMA(Module):
 
         if not exists(self.ema_model):
             try:
-                self.ema_model = copy.deepcopy(model)
+                self.ema_model = deepcopy(model)
             except:
                 print('Your model was not copyable. Please make sure you are not using any LazyLinear')
                 exit()
 
         self.ema_model.requires_grad_(False)
 
+        # parameter and buffer names
+
         self.parameter_names = {name for name, param in self.ema_model.named_parameters() if param.dtype in [torch.float, torch.float16]}
         self.buffer_names = {name for name, buffer in self.ema_model.named_buffers() if buffer.dtype in [torch.float, torch.float16]}
+
+        # tensor update functions
+
+        self.inplace_copy = partial(inplace_copy, auto_move_device = allow_different_devices)
+        self.inplace_lerp = partial(inplace_lerp, auto_move_device = allow_different_devices)
+
+        # updating hyperparameters
 
         self.update_every = update_every
         self.update_after_step = update_after_step
@@ -87,6 +113,12 @@ class EMA(Module):
 
         self.ignore_names = ignore_names
         self.ignore_startswith_names = ignore_startswith_names
+
+        # whether to manage if EMA model is kept on a different device
+
+        self.allow_different_devices = allow_different_devices
+
+        # init and step states
 
         self.register_buffer('initted', torch.tensor(False))
         self.register_buffer('step', torch.tensor(0))
@@ -115,18 +147,22 @@ class EMA(Module):
             yield name, buffer
 
     def copy_params_from_model_to_ema(self):
+        copy = self.inplace_copy
+
         for (_, ma_params), (_, current_params) in zip(self.get_params_iter(self.ema_model), self.get_params_iter(self.model)):
-            ma_params.data.copy_(current_params.data)
+            copy(ma_params.data, current_params.data)
 
         for (_, ma_buffers), (_, current_buffers) in zip(self.get_buffers_iter(self.ema_model), self.get_buffers_iter(self.model)):
-            ma_buffers.data.copy_(current_buffers.data)
+            copy(ma_buffers.data, current_buffers.data)
 
     def copy_params_from_ema_to_model(self):
+        copy = self.inplace_copy
+
         for (_, ma_params), (_, current_params) in zip(self.get_params_iter(self.ema_model), self.get_params_iter(self.model)):
-            current_params.data.copy_(ma_params.data)
+            copy(current_params.data, ma_params.data)
 
         for (_, ma_buffers), (_, current_buffers) in zip(self.get_buffers_iter(self.ema_model), self.get_buffers_iter(self.model)):
-            current_buffers.data.copy_(ma_buffers.data)
+            copy(current_buffers.data, ma_buffers.data)
 
     def get_current_decay(self):
         epoch = (self.step - self.update_after_step - 1).clamp(min = 0.)
@@ -156,6 +192,7 @@ class EMA(Module):
 
     @torch.no_grad()
     def update_moving_average(self, ma_model, current_model):
+        copy, lerp = self.inplace_copy, self.inplace_lerp
         current_decay = self.get_current_decay()
 
         for (name, current_params), (_, ma_params) in zip(self.get_params_iter(current_model), self.get_params_iter(ma_model)):
@@ -166,10 +203,10 @@ class EMA(Module):
                 continue
 
             if name in self.param_or_buffer_names_no_ema:
-                ma_params.data.copy_(current_params.data)
+                copy(ma_params.data, current_params.data)
                 continue
 
-            ma_params.data.lerp_(current_params.data, 1. - current_decay)
+            lerp(ma_params.data, current_params.data, 1. - current_decay)
 
         for (name, current_buffer), (_, ma_buffer) in zip(self.get_buffers_iter(current_model), self.get_buffers_iter(ma_model)):
             if name in self.ignore_names:
@@ -179,10 +216,10 @@ class EMA(Module):
                 continue
 
             if name in self.param_or_buffer_names_no_ema:
-                ma_buffer.data.copy_(current_buffer.data)
+                copy(ma_buffer.data, current_buffer.data)
                 continue
 
-            ma_buffer.data.lerp_(current_buffer.data, 1. - current_decay)
+            lerp(ma_buffer.data, current_buffer.data, 1. - current_decay)
 
     def __call__(self, *args, **kwargs):
         return self.ema_model(*args, **kwargs)
